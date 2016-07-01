d135210fa1c368f128d93f3208fcccd4c9170cb1
bneiluj <neiluj_08@hotmail.com>
2016-07-01 12:26:52 +0100

    Modify index boitelate

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,12 @@
+import { FETCH_POSTS } from '../actions/index';
+
+const INITIAL_STATE = {};
+
+// By default our piece of post will be initiate to an empty object
+export default function (state = INITIAL_STATE, action ) {
+  switch(action.type) {
+
+  }
+  default:
+    return state;
+}
