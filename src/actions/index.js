654520f9ef149dbf9bd78c901a427fd933284ba7
bneiluj <neiluj_08@hotmail.com>
2016-07-04 15:41:09 +0100

    Add fetching feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=1234Test';
 
-export function fetchPosts () => {
+export function fetchPosts () {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,12 +1,20 @@
 import { FETCH_POSTS } from '../actions/index';
 
-const INITIAL_STATE = {};
+// The post is the individual post
+// All is all posts
+const INITIAL_STATE = {
+  all: [],
+  post: null
+};
 
 // By default our piece of post will be initiate to an empty object
 export default function (state = INITIAL_STATE, action ) {
   switch(action.type) {
-
-  }
+    case FETCH_POSTS:
+      // New object, and then take whatever is the current state and add
+      // the data
+      return { ...state, all: action.payload.data};
   default:
     return state;
+  }
 }
