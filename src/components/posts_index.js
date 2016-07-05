75448dc20303a7aefb60979d8014fcd04dcb5299
bneiluj <neiluj_08@hotmail.com>
2016-07-05 12:16:19 +0100

    Add redux link module

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 // Function based component
 // export default () => {
@@ -19,8 +20,17 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of blog posts</div>
-    )
+      <div>
+        <div className="text-xs-right">
+          <Link
+            to="/posts/new"
+            className="btn btn-primary">
+            Add a post
+          </Link>
+        </div>
+          List of blog posts
+      </div>
+    );
   }
 }
 // export default PostsIndex;
