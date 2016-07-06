8e962fe8d201e5448c67f15f0904fc145cd0a2ce
bneiluj <neiluj_08@hotmail.com>
2016-07-06 11:34:55 +0100

    Update reducers and actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
+
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=1234Test';
 
@@ -12,4 +14,14 @@ export function fetchPosts () {
     payload: request
   }
 
+}
+
+// let's define the action creator for the post post.
+export function createPost(props) {
+  const request = axios.post(`${ROOt_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+import {reducer as formReducer } from 'redux-form';
+// this syntax means grab reducer from  redux-form and change name to formReducer
 
 const rootReducer = combineReducers({
   // state: (state = {}) => state
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
