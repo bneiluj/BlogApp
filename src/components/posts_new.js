cca1758934983b5791380276ce57076eb853ff27
bneiluj <neiluj_08@hotmail.com>
2016-07-13 11:56:18 +0100

    Modify posts new

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+    // Try avoid context
+    // Defining an object on the post new class
+    // The router will give use the context from index.js
+    // It's like props
+    // Just give all the properties from all the parents components
+    static contextTypes = {
+      router: PropTypes.object
+    };
+
+    onSubmit(props) {
+      this.props.createPost(props);
+    }
     render() {
       const { fields: {title, categories, content }, handleSubmit } = this.props;
       // same as const handleSubmit = this.props.handleSubmit;
       // same as const titile = this.props.fields.title;
       return (
         // the handleSubmit needs an action creator
-        <form onSubmit={ handleSubmit(this.props.createPost) }>
+        // <form onSubmit={ handleSubmit(this.props.createPost) }>
+        // need to bind the context
+        // the bind(this) means it will pass properties from the form 
+        <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
           <h3>Create a new Post</h3>
           <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
             <label>Title</label>
