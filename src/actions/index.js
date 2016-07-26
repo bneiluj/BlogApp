71a8a3f6aff9191a62a85317217b6cfbca2695eb
bneiluj <neiluj_08@hotmail.com>
2016-07-26 18:19:35 +0100

    Delete post functionnality

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=1234Test';
@@ -38,6 +39,18 @@ export function fetchPost(id) {
 
   return {
     type: FETCH_POST,
+    payload: request
+  }
+}
+
+/**
+ * Delete a single post 
+ */
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
     payload: request
   }
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
-
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
+  }
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id)// the action creator is a promise
+        .then(() => {
+          this.context.router.push('/');
+        });
   }
   render () {
     const { post } = this.props; //identical to: const post = this.props.post
 
-    if (!this.props.post) {
+    if (!post) {
       return <div>Loading single post</div>
     }
     return (
       <div>
+        <Link to="/">Back to index</Link>
+        <button className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
-        <h3>Categories: {post.categories}</h3>
-        <h3>{post.content}</h3>
+        <h4>Categories: {post.categories}</h4>
+        {post.content}
       </div>
     )
   }
@@ -28,4 +42,4 @@ function mapDispatchToProps(state) {
   return { post: state.posts.post };
 }
 
-export default connect(mapDispatchToProps, { fetchPost })(PostsShow);
+export default connect(mapDispatchToProps, { fetchPost, deletePost })(PostsShow);
